caa61ae27d0d38e0ed959018fd7a03364ab93def
DJSablosky <DJSablosky@msn.com>
2018-11-07 18:09:10 -0600

    Clean up and refactor bucket management code

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -12,20 +12,14 @@ Webotron automates the process of deploying static websites to AWS.
 - Configure a Content Delivery Network and SSL with AWS CloudFront
 """
 
-from pathlib import Path
-import mimetypes
 
 import boto3
-from botocore.exceptions import ClientError
-<<<<<<< HEAD
 import click
-=======
-from pathlib import Path
-import mimetypes
->>>>>>> 6db30be7463895435e3d14b62f75b38187d3e658
+
+from bucket import BucketManager
 
 session = boto3.Session(profile_name='mycli')
-s3 = session.resource('s3')
+bucket_manager = BucketManager(session)
 
 
 @click.group()
@@ -37,7 +31,7 @@ def cli():
 @cli.command('list-buckets')
 def list_buckets():
     """List all s3 buckets."""
-    for bucket in s3.buckets.all():
+    for bucket in bucket_manager.all_buckets():
         print(bucket)
 
 
@@ -45,7 +39,7 @@ def list_buckets():
 @click.argument('bucket')
 def list_bucket_objects(bucket):
     """List objects in an s3 bucket."""
-    for obj in s3.Bucket(bucket).objects.all():
+    for obj in bucket_manager.all_objects(bucket):
         print(obj)
 
 
@@ -53,56 +47,11 @@ def list_bucket_objects(bucket):
 @click.argument('bucket')
 def setup_bucket(bucket):
     """Create and configure S3 bucket."""
-    s3_bucket = None
+    s3_bucket = bucket_manager.init_bucket(bucket)
+    bucket_manager.set_policy(s3_bucket)
+    bucket_manager.configure_website(s3_bucket)
 
-    try:
-        s3_bucket = s3.create_bucket(
-            Bucket=bucket,
-            CreateBucketConfiguration={
-                'LocationConstraint': session.region_name}
-        )
-    except ClientError as error:
-        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
-            s3_bucket = s3.Bucket(bucket)
-        else:
-            raise error
-
-    policy = """
-    {
-        "Version":"2012-10-17",
-        "Statement":[{
-            "Sid":"PublicReadObject",
-            "Effect":"Allow",
-            "Principal":"*",
-                "Action":["s3:GetObject"],
-                "Resource":["arn:aws:s3:::%s/*"]
-        }]
-    }
-    """ % s3_bucket.name
-    policy = policy.strip()
-
-    pol = s3_bucket.Policy()
-    pol.put(Policy=policy)
-
-    s3_bucket.Website().put(WebsiteConfiguration={
-        'ErrorDocument':  {
-            'Key':  'error.html'
-        },
-        'IndexDocument':  {
-            'Suffix':  'index.html'
-        }
-    })
-
-def upload_file(s3_bucket, path, key):
-    """Upload path to s3_bucket at key."""
-    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
-    s3_bucket.upload_file(
-        path,
-        key,
-        ExtraArgs={
-            'ContentType':  content_type
-        }
-    )
+    return
 
 
 @cli.command('sync')
@@ -110,45 +59,8 @@ def upload_file(s3_bucket, path, key):
 @click.argument('bucket')
 def sync(pathname, bucket):
     """Sync contents to PATHNAME to BUCKET."""
-    s3_bucket = s3.Bucket(bucket)
+    bucket_manager.sync(pathname, bucket)
 
-    root = Path(pathname).expanduser().resolve()
-
-    def handle_directory(target):
-        for p in target.iterdir():
-            if p.is_dir():
-                handle_directory(p)
-            if p.is_file():
-                upload_file(s3_bucket, str(p), str(p.relative_to(root)))
-
-    handle_directory(root)
-
-
-def upload_file(s3_bucket, path, key):
-    s3_bucket.upload_file(
-        path,
-        key,
-        ExtraArgs={
-            'ContentType':  'text/html'
-        }
-    )
-
-@cli.command('sync')
-@click.argument('pathname', type=click.Path(exists=True))
-@click.argument('bucket')
-def sync(pathname, bucket):
-    "Sync contents to PATHNAME to BUCKET"
-    s3_bucket = s3.Bucket(bucket)
-
-
-    root = Path(pathname).expanduser().resolve()
-
-    def handle_directory(target):
-        for p in target.iterdir():
-            if p.is_dir():  handle_directory(p)
-            if p.is_file():  upload_file(s3_bucket, str(p), str(p.relative_to(root)))
-
-    handle_directory(root)
 
 if __name__ == '__main__':
     cli()
